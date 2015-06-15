4571d4f65664350f7990d8b6410d670d7d8c6cf6
Andrew.Gibson <andrew.gibson@verint.com>
2015-06-15 12:57:15 +0100

    Added first XSS filter evasion test

---
+++ HtmlSanitizer.Tests/OWASP/XSS/XSS_Locator/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Ganss.XSS.Tests.OWASP.XSS.XSS_Locator
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class HtmlSanitizerTests
+    {
+        private string Input { get; set; }
+        private string Expected { get; set; }
+
+        [SetUp]
+        public void ReadResources()
+        {
+            Input = ReadResource("input.html");
+            Expected = ReadResource("expected.html");
+        }
+
+        private static string ReadResource(string name)
+        {
+            var resourceName = typeof (HtmlSanitizerTests).Namespace + string.Format(".{0}", name);
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            return stream == null ? string.Empty : new StreamReader(stream).ReadToEnd();
+        }
+
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public void XSSLocatorTest()
+        {
+            var sanitizer = new HtmlSanitizer();
+            var actual = sanitizer.Sanitize(Input);
+            Assert.That(actual, Is.EqualTo(Expected));
+        }
+    }
+}
